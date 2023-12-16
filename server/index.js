50cc52a0191d6524f31dbf5312b45372b3e6cc6e
mortale2004 <pawanmortale111@gmail.com>
2023-12-16 19:25:30 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+const io = socketIO(server);
+const path = require("path");
+
+let users = [];
+
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+io.on("connection", (client) => {
+    
+    
+    console.log("current users = ", users.length);
+    
+    client.on("new-user-joined", (data) => {
+        if (data.name===null) return;
+
+        users.push(data);
+        client.broadcast.emit("user-joined", {users: users.map(u=>u.name),  name:data.name});
+
+        console.log("current users = ", users.length);
+    })
+
+    client.on("send", data => {
+        const user = users.find(user=> user.id===client.id);
+        client.broadcast.emit("receive", { message: data.message, id:client.id, name:user.name});
+    })
+
+    client.on("disconnect", () => {
+
+        const leftUser = users.find(user=> user.id===client.id);
+        client.broadcast.emit("user-left", {name:leftUser?.name, users: users.map(u=>u.name)})
+        users = users.filter(user=> user.id!==client.id);
+
+        console.log("current users = ", users.length);
+    })
+
+});
+
+server.listen(process.env.PORT, (req, res) => {
+    console.log(`Server Started on ${process.env.REACT_APP_END_POINT}`);
+})
